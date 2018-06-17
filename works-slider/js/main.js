711b83872a92173e4b1a951e265b4513309f3ec5
evdokimenko <33295500+evdokimenko@users.noreply.github.com>
2018-06-17 23:19:29 +0300

    add new project works-slider

---
+++ works-slider/js/main.js
@@ -0,0 +1,11 @@
+;(function($){
+    $(function () {
+        $('.mySlider').slick({
+            infinite: true,
+            arrows: true,
+            slidesToShow: 3,
+            slidesToScroll: 1
+
+        });
+    })
+})(jQuery);
